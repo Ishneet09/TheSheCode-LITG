79d68c6a8ae867991b3f1b8195eca82c83b83454
Ishneet09 <43916654+Ishneet09@users.noreply.github.com>
2019-03-29 19:17:53 +0530

    Registration and Login Classes added

---
+++ app/src/main/java/Login.java
@@ -0,0 +1,22 @@
+public class Login {
+    String email;
+    String password;
+
+    public Login(){
+
+    }
+
+    public Login(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+}
